1fd890b30441852d73828041c44a25e8759fd65c
br00ta1 <mail2naresh@gmail.com>
2011-04-01 19:29:08 -0400

    Adding new files

---
+++ siteConfig.py
@@ -0,0 +1,25 @@
+'''
+This module declares global configuration parameters.
+
+Author          :                Naresh Pratap Singh
+E-Mail          :                MAIL2NARESH@GMAIL.COM
+Date            :                Jan 18 2011
+Project Advisor :                Prof. Steven Skiena
+'''
+
+dbHost                  = 'localhost'
+dbUser                  = 'root'
+dbPass                  = 'passwd'
+dbName                  = 'gamut'
+
+debugEnabled            = True
+fileSuffix              = '.lifelogger'
+logFileDir              = programDir + 'Logs'
+programDir              = '/home/naresh/git/Naresh/LifeParser/'
+repoRoot                = programDir + 'Data/'
+analysisDir             = programDir + 'Analysis/'
+analysisSentSuffix      = '.sent'
+analysisRecvSuffix      = '.recv'
+siteUid                 = 2016
+sentimentMainFile       = programDir + 'Sentiments/sentiwords.list'
+sentimentModifiersFile  = programDir + 'Sentiments/modifiers'
---
+++ testDaemon.py
@@ -0,0 +1,27 @@
+import daemon
+import time
+import glob
+import logging
+import logging.handlers
+
+class LifeParserDaemon(daemon.Daemon):
+    logger              = None
+
+    def __init__(self):
+        self.logger     = logging.getLogger('LifeParserLogger')
+        self.logger.setLevel(logging.DEBUG)
+        handler         = logging.handlers.RotatingFileHandler('/tmp/lifeparser.log', maxBytes=10000000, backupCount=5)
+        self.logger.addHandler(handler)
+        daemon.Daemon.__init__(self, '/tmp/pidfile')
+
+    def run(self):
+#fileName        =   '/tmp/lifeparser.log'
+#       logging.basicConfig(filename=fileName, level=logging.DEBUG)
+#       logging.debug('Go to log file')
+        self.logger.debug('Testing Log')
+        time.sleep(1000)
+
+if __name__ == '__main__':
+    lPD = LifeParserDaemon()
+    lPD.start()
+
